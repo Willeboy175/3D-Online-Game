37ef8aea89fa5c5967fc2fd1cf0a705b7c53bc82
Willeboy175 <william.strandberg@elev.ga.lbs.se>
2024-03-07 11:26:46 +0100

    starting on save system

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSystem
+{
+    ObjectContainer container;
+
+    void Save<T>(string saveKey, T saveValue)
+    {
+        
+    }
+
+    void Load<T>(string saveKey)
+    {
+        
+    }
+}
