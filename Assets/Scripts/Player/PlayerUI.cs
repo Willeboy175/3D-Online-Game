cc58a3301e6cb6b04885d764d575ba339e169686
Willeboy175 <william.strandberg@elev.ga.lbs.se>
2024-08-26 15:42:37 +0200

    Open and close inventory

--- Assets/Scripts/Player/PlayerUI.cs
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerUI : MonoBehaviour
@@ -9,12 +8,36 @@ public class PlayerUI : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI promptText;
     private GameObject textObject;
+    private GameObject playerInventory;
+    private bool isActive = false;
 
     // Start is called before the first frame update
     void Start()
     {
         textObject = GameObject.FindGameObjectWithTag("PromptText");
         promptText = textObject.GetComponent<TextMeshProUGUI>();
+        playerInventory = GameObject.FindGameObjectWithTag("InventoryPanel");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            playerInventory.SetActive(isActive);
+
+            if (isActive)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = true;
+            }
+
+            isActive = !isActive;
+        }
     }
 
     //Updates promptText
